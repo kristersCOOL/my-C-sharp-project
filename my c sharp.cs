4d4a5a04f47bfdddebf1eda3422a288e69c338cc
kristersCOOL <134638197+kristersCOOL@users.noreply.github.com>
2023-12-05 13:59:24 +0200

    c# its cs to make it an exe

---
+++ my c sharp.cs
@@ -0,0 +1,44 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+      // text of the start
+      Console.WriteLine("Welcome programmer")
+      
+      // Prompt the user to enter something
+        Console.WriteLine("Enter your name pls:");
+        
+        // Read the user input and store it in a variable
+        string userInput = Console.ReadLine();
+        
+        // Display what the user entered
+       Console.WriteLine("oh hi " + userInput);
+       
+      
+      Console.WriteLine("wanna exit?");
+        
+        while (true)
+        {
+            // Display options
+            Console.WriteLine("no");
+            Console.WriteLine("exit")
+
+            // Get user input
+            string choice = Console.ReadLine();
+
+            // Process user input
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("ok");
+                    
+                    Console.Write.Line("but i can't stay that long i have other stuff to do like update, so bye")
+                    return;
+
+                case "2":
+                    Console.WriteLine("ok then exit, goodbye!");
+                    return;
+    }
+}
